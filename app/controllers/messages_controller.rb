97dd25c986556b1ca4183bcbd61bfd77fc7d7bb4
aruga-eri <eri0725.t@gmail.com>
2024-07-27 16:23:02 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,10 +20,5 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-
-  def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
-
-
